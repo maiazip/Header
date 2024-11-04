6a7fe63af3e3297c2c131ad09eaeef21d1b82f18
maia.zip <maia.zip@hotmail.com>
2024-11-04 17:23:02 -0300

    t

--- src/javascript/scripts.js
+++ src/javascript/scripts.js
@@ -1,4 +1,4 @@
-function showSidebar(){
+function showSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'flex'
 }
@@ -6,4 +6,14 @@ function showSidebar(){
 function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
      sidebar.style.display = 'none' 
+}
+
+function showMoreProjects() {
+    const moreProjects = document.querySelectorAll('.moreProjects')
+    for (const project of moreProjects) {
+        project.style.display = 'grid';
+    }
+
+    const moreProjectsButton = document.querySelector('.moreProjectsButton')
+    moreProjectsButton.style.display = 'none';
 }
---
+++ src/javascript/typed.js
@@ -0,0 +1,6 @@
+// var typed = new Typed(".auto-type", {
+//     strings : ["Designer", "React"],
+//     typeSpeed : 50,
+//     backSpeed : 50,
+//     loop : true
+// })
